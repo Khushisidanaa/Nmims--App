10f548e3ba050bdca270b7c4b1a880bc4ef846af
Khushi Sidana <khushisidana@Khushis-MacBook-Air-3.local>
2023-11-25 13:30:40 -0600

    first commit

--- App.js
+++ App.js
@@ -10,26 +10,11 @@ import * as Font from 'expo-font';
 import LoginScreen from './screens/LoginScreen';
 import SignUp from './screens/SignUp';
 import StudentLandingScreen from './screens/StudentLandingScreen';
+import StudentLogin from './screens/StudentLogin';
 
 
 export default function App() {
   const Stack = createStackNavigator();
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  useEffect(() => {
-    async function loadFonts() {
-      await Font.loadAsync({
-        'custom-font': require('./assets/fonts/Raleway-Black.ttf'),
-      });
-      setFontsLoaded(true);
-    }
-
-    loadFonts();
-  }, []);
-
-  // if (!fontsLoaded) {
-  //   return null; // or a loading screen
-  // }
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -37,7 +22,9 @@ export default function App() {
         <Stack.Screen name="LandingScreen" component={LandingScreen} options={{headerShown:false}}/>
         <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown:false}}/>
         <Stack.Screen name="SignUp" component ={SignUp} options={{headerShown:false}} />
+        <Stack.Screen name="StudentLogin" component={StudentLogin} options={{headerShown:false}} />
         <Stack.Screen name="StudentLandingScreen" component ={StudentLandingScreen} options={{headerShown:false}} />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
